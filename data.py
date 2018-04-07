0e4bd9da0e84e2cd19272b4a3df2b0d4da699f1c
ZHIYONGCEHN <827142031@qq.com>
2018-04-07 13:27:27 +0800

    add data.py to handle the wind api

---
+++ data.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from WindPy import *
+
+class WindDataHandler(object):
+    """
+    """
+    def __init__(self):
+        w.start()
+    
+
+    def get_historical_data(self, symbols, fields, start_date, end_date):
+        """
+        """
+        w.wsd()
+
